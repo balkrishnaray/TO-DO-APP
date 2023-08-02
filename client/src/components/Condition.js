50d09dd95b7c0d3b9230e165e2847759de077992
balkrishnray <balkrishnaroy31@gmail.com>
2023-08-02 13:43:04 +0545

    if else condition for goal

---
+++ client/src/components/Condition.js
@@ -0,0 +1,17 @@
+function Goal(props) {
+  const isGoal = props.isGoal;
+
+  if (isGoal) {
+    return <MadeGoal />;
+  }
+  return <MissedGoal />;
+}
+
+function MissedGoal() {
+  return <h1>MISSED!</h1>;
+}
+function MadeGoal() {
+  return <h1>Goal!</h1>;
+}
+
+export default Goal;
