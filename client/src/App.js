24a3813dbd8a3f203f1202a30a298480be3eeb08
balkrishnray <balkrishnaroy31@gmail.com>
2023-08-02 14:04:54 +0545

    function updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import Title from "./components/Title";
 import Example from "./components/Example";
+import Goal from "./components/Condition";
+import Garage from "./components/LogicalOpearator";
+import Goals from "./components/Ternary";
 
 function App() {
+  const cars = ["Ford", "BMW", "Audi"];
   return (
     <>
       <Title name="ToDo App" />
       <Example />
+      <Goal isGoal={false} />
+      <Garage cars={cars} />
+      <Goals isGoal={false} />
     </>
   );
 }
