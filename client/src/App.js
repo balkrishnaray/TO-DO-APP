ee7e23fdb76b03be05f181bddc56d55816eb1dff
balkrishnray <balkrishnaroy31@gmail.com>
2023-08-02 14:13:56 +0545

    function modified

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Example from "./components/Example";
 import Goal from "./components/Condition";
 import Garage from "./components/LogicalOpearator";
 import Goals from "./components/Ternary";
+import Garages from "./components/List";
 
 function App() {
   const cars = ["Ford", "BMW", "Audi"];
@@ -13,6 +14,7 @@ function App() {
       <Goal isGoal={false} />
       <Garage cars={cars} />
       <Goals isGoal={false} />
+      <Garages brand={cars} />
     </>
   );
 }
